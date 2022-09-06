8661ac664cda097c21a62e10f67f2c3de8e7d4c5
SaranRJ <90471894+SaranRJ@users.noreply.github.com>
2022-09-06 13:08:17 +0530

    Add files via upload

---
+++ Looping/src/org/forloop/LearnFor.java
@@ -0,0 +1,105 @@
+package org.forloop;
+
+import java.util.Scanner;
+
+public class LearnFor {
+
+	public static void main(String[] args) {
+     
+
+	 int[] a= {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	
+	 int l = a.length;
+	
+		for (int i = 1; i < a.length; i++) {
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//--------Que 7----------
+//		int f = 0;
+//		int num = 100;
+//		for (int i = 0; i <=num; i++) {
+//			int f1= 0+i;
+//			f=f+f1;
+//			
+//		}
+//		System.out.println("Fibonnaci series:"+f);
+	
+		//---------Que 6--------
+//		int fact=1;
+//		int num = 5;
+//		
+//		for (int i =1; i <=num; i++) {
+//			fact = i*fact;
+//		}
+//		
+//		System.out.println("The factorial of a number 5:"+fact);
+		
+		
+		//---------Que 5---------
+//		int count = 0;
+//		for (int i =1; i <=100; i++) {
+//			if(i%2==0) {
+//				count = count + 1;
+//				System.out.println("Total Even numbers["+i+"]:"+count);	
+//		}
+//	}
+	
+		//------Que 4-------
+//		int sum = 0;
+//		for (int i = 0; i <=100; i++) {
+//			if(i%2!=0) {
+//				sum=sum+i;
+//				System.out.println("sum of odd numbers is:"+sum);
+//			}
+//		}	
+	
+		//--------Que 3-------
+//		for (int i = 1; i<=100; i++) {
+//			if(i%2==0) {
+//				System.out.println("Even Numbers:"+ i);
+//			}
+//		}
+		
+		
+		//----Que 2------
+//		int a = 10;
+//		if(a%2==0) {
+//			System.out.println("The number is Even");
+//		}
+		
+		
+//		//-----Que 1----------
+//     	Scanner s = new Scanner(System.in);
+//		int age = s.nextInt();
+//		
+//		if(age>=18) {
+//			System.out.println("You are eligible to vote");
+//		}
+//		else {
+//			System.out.println("You are not eligible to vote");
+//		}
+	  
+	
+	}
+}
+
+
